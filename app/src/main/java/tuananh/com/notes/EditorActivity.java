4ae2881b52f04ccb880c09eb9ec103ebd7f0c171
anhlt570 <anhletuan128@gmail.com>
2016-12-22 18:23:28 +0700

    - Edit background for note-viewer on Main screen + Add click listener for note-viewer

--- app/src/main/java/tuananh/com/notes/EditorActivity.java
+++ app/src/main/java/tuananh/com/notes/EditorActivity.java
@@ -11,21 +11,28 @@ import android.widget.EditText;
  * Created by anh.letuan2 on 12/22/2016.
  */
 
-public class EditorActivity extends RootActivity{
+public class EditorActivity extends RootActivity
+{
+    public EditText editNote ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.note_editor);
+        editNote = (EditText) findViewById(R.id.et_note);
 
         Button buttonDone = (Button) findViewById(R.id.bt_done_edit);
         buttonDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText newNote = (EditText) findViewById(R.id.et_note);
-                myDatabase.addNote(newNote.getText().toString());
+                myDatabase.addNote(editNote.getText().toString());
                 goToMain();
             }
         });
+
+        Intent intent = getIntent();
+        String messageFromMain = intent.getStringExtra(MainActivity.MESSAGE_TO_EDITOR);
+        if(messageFromMain!=null) editNote.setText(messageFromMain);
     }
 
     public void goToMain()
--- app/src/main/java/tuananh/com/notes/MainActivity.java
+++ app/src/main/java/tuananh/com/notes/MainActivity.java
@@ -6,16 +6,18 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.TypedValue;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends RootActivity {
 
     public int m_currentLayout;
-
+    protected final static String MESSAGE_TO_EDITOR ="MessageToEditor";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +29,7 @@ public class MainActivity extends RootActivity {
             @Override
             public void onClick(View view) {
                 Log.d(TAG, "onClick: ");
-                goToEditor();
+                goToEditor(null);
             }
         });
 
@@ -41,6 +43,8 @@ public class MainActivity extends RootActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        LinearLayout listNotes = (LinearLayout) findViewById(R.id.list_notes);
+        listNotes.removeAllViews();
         Cursor cursor = myDatabase.readDatabase();
         if(cursor.moveToFirst())
         {
@@ -56,23 +60,32 @@ public class MainActivity extends RootActivity {
         }
     }
 
-    void addNoteView(String newNote)
+    void addNoteView(final String newNote)
     {
         LinearLayout listNotes = (LinearLayout) findViewById(R.id.list_notes);
         TextView note = new TextView(this);
-        ViewGroup.LayoutParams layoutParam = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        note.setBackgroundResource(R.drawable.note_background);
+        LinearLayout.LayoutParams layoutParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout .LayoutParams.WRAP_CONTENT);
+        layoutParam.setMargins(20,7,20,7);
         note.setLayoutParams(layoutParam);
         note.setText(newNote);
-        note.setPadding(7,7,7,7);
-        note.setTextSize(TypedValue.COMPLEX_UNIT_PT,11);
+        note.setPadding(10,20,0,20);
+        note.setTextSize(TypedValue.COMPLEX_UNIT_PT,10);
         note.setTextColor(Color.parseColor("#000000"));
-        note.setBackgroundColor(Color.parseColor("#f9e954"));
+        note.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToEditor(newNote);
+            }
+        });
         listNotes.addView(note);
     }
 
-   public void goToEditor()
+   public void goToEditor(String currentNote)
    {
        Intent intent = new Intent(this,EditorActivity.class);
+       EditText editNote = (EditText) findViewById(R.id.et_note);
+       if(currentNote!=null) intent.putExtra(MESSAGE_TO_EDITOR,currentNote);
        startActivity(intent);
    }
 
